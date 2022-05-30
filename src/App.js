f4a825753f59a33400a5dbc85ad2fed831533d5b
birendra20 <birendragupta20@gmail.com>
2022-05-30 07:53:34 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,26 +1,85 @@
+import { useEffect, useState } from "react";
 import "./App.css";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
 
 import Circle from "./components/Circle";
 
 function App() {
+  const [balloons, setBalloons] = useState([]);
+  const [added, setAdded] = useState([]);
+  const [shootNum, setShootNum] = useState("");
+
+  //generates an array of random colors
+  const colorGen = () => {
+    let arr = [];
+    for (let i = 1; i <= 5; i++) {
+      arr.push([`#${Math.floor(Math.random() * 16777215).toString(16)}`, i]);
+    }
+    setBalloons(arr);
+  };
+
+  useEffect(() => {
+    colorGen();
+  }, []);
+
+  //to shoot the balloons to empty div
+  const handleClick = () => {
+    let balloon = balloons.find((el) => el[1] == shootNum);
+    if (!balloon) {
+      alert("enter a valid number");
+      return;
+    }
+    const newArr = balloons.filter((el) => el[1] != shootNum);
+    setBalloons([...newArr]);
+    setAdded([...added, balloon]);
+  };
+
+  //to add the balloon back to original container
+  const handleAdded = (el) => {
+    let newArr = added.filter((elem) => el[1] !== elem[1]);
+    setAdded([...newArr]);
+    setBalloons([...balloons, el].sort((a, b) => a[1] - b[1]));
+  };
   return (
     <>
-      <div className="App">
-        <div className="empty_div"></div>
-
+      <div className="app">
         <div className="circle">
-          <Circle />
+          {balloons.map((el) => (
+            <Circle key={el[1]} color={el[0]} num={el[1]}></Circle>
+          ))}
         </div>
-        <form className="shoot">
-          <input
-            // onChange={(e) => setShootNum(e.target.value)}
-            id="outlined-basic"
-            label="enter a number"
-            variant="outlined"
+
+        <div
+          className="empty"
+          style={{
+            display: "flex",
+            gap: "10px",
+            flexWrap: "wrap",
+            padding: "10px",
+          }}
+        >
+          {added.map((el) => {
+            return (
+              <div onClick={() => handleAdded(el)}>
+                <Circle key={el[1]} color={el[0]} num={el[1]}></Circle>
+              </div>
+            );
+          })}
+        </div>
+
+        <div className="shoot">
+          <TextField
+            onChange={(e) => setShootNum(e.target.value)}
+            id="standard-basic"
+            label="Standard"
+            variant="standard"
             type="number"
           />
-          <submit type="button">shoot</submit>
-        </form>
+          <Button onClick={handleClick} variant="contained">
+            shoot
+          </Button>
+        </div>
       </div>
     </>
   );
--- src/components/Circle.jsx
+++ src/components/Circle.jsx
@@ -1,7 +1,25 @@
 import React from "react";
+import "./circle.css";
 
-const Circle = () => {
-  return <></>;
+const Circle = ({ color, num }) => {
+  return (
+    <>
+      <div
+        className="balloons"
+        style={{
+          width: "100px",
+          height: "100px",
+          borderRadius: "50%",
+          background: color,
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <div>{num}</div>
+      </div>
+    </>
+  );
 };
 
 export default Circle;
